e5dff612dd590af2f4393bfbe3e8de6980536b6c
chavierto <hola@xavierlujan.com>
2021-03-04 15:10:40 -0500

    Create PostsPanel component
        - Create PostsPanel component
        - Reorganize Nav components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navbar from './Navbar/Navbar';
 import InfoPanel from './InfoPanel';
 
 import './App.css';
+import PostsPanel from './PostsPanel';
 
 function App() {
 	return (
 		<div className='App'>
 			<nav>
 				<Navbar />
-				<InfoPanel />
 			</nav>
+			<InfoPanel />
+			<PostsPanel />
 		</div>
 	);
 }
---
+++ src/PostsPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PostsPanel(posts) {
+	return (
+		<div>
+			<h1>Projects</h1>
+		</div>
+	);
+}
+
+export default PostsPanel;
