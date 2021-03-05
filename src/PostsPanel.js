488eecd263de48c4e7472ed88d69426537d21bda
chavierto <hola@xavierlujan.com>
2021-03-05 00:02:52 -0500

    Add conditional rendering to post media
        - Create a function that checks if the post has a videoLink address
        - If if does, and is not null, display youtube video
        - If it doesn't, or is null, display img

--- src/PostsPanel.js
+++ src/PostsPanel.js
@@ -2,19 +2,36 @@ import React from 'react';
 import posts from './posts';
 
 function PostsPanel() {
+	// Function for conditional display of video vs. image
+	function mediaChooser(post) {
+		if (post.videoLink && post.videoLink != null) {
+			return (
+				<iframe
+					title={post.title}
+					class='media'
+					src={post.videoLink}
+					frameborder='0'
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+					allowfullscreen></iframe>
+			);
+		} else {
+			return <img alt={post.title} className='media' src={post.img}></img>;
+		}
+	}
+
 	return (
+		// map out individual post from 'posts'
 		<div>
 			<h1>Projects:</h1>
 			{posts.map((post) => {
 				return (
 					<div key={post.id}>
-						<img alt={post.title} className='media'
-                        src={post.img}></img>
 						<h2>
 							<a href={post.url} target='_blank' rel='noreferrer'>
 								{post.title}
-							</a>
+							</a>{' '}
 						</h2>
+						{mediaChooser(post)}
 						<p>{post.body}</p>
 					</div>
 				);
--- src/posts.js
+++ src/posts.js
@@ -16,7 +16,7 @@ const posts = [
 			'https://user-images.githubusercontent.com/62493112/95257720-bf6cf100-07f2-11eb-9870-efd5ebd6b13e.png',
 		body:
 			"Tern is a to-do app I made in collaboration with three other developers. We wanted to have a place where we could add to-do items and be able to edit them and mark them as complete. We then though about adding a number value so we could prioritize items and give them a visual reference of the priority. Finally, we added a to-do view and a completed view, in order to only see relevant to-do's. I worked primarily on the backend, where we used MongoDB, Mongoose and Express (and React on the frontend), resulting in the ability to perform full CRUD operations to the database in order to add, edit and delete to-do items.",
-		videoLink: 'https://www.youtube.com/embed/-OSjIRVcOYA',
+		videoLink: null,
 	},
 ];
 
