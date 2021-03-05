574013f2774d1eab6100cee8952442b485534295
chavierto <hola@xavierlujan.com>
2021-03-05 00:23:25 -0500

    Update PostsPanel
        - Add link to deployed app & Github repo on posts
        - Add github key to posts.js

--- src/PostsPanel.js
+++ src/PostsPanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import posts from './posts';
 
 function PostsPanel() {
-	// Function for conditional display of video vs. image
+	// Function mediaChooser defines conditional rendering of post video vs. image
 	function mediaChooser(post) {
 		if (post.videoLink && post.videoLink != null) {
 			return (
@@ -21,18 +21,35 @@ function PostsPanel() {
 
 	return (
 		// map out individual post from 'posts'
+		// this can be extracted into its own 'Post' component
 		<div>
 			<h1>Projects:</h1>
 			{posts.map((post) => {
 				return (
 					<div key={post.id}>
+						{/* post title */}
 						<h2>
 							<a href={post.url} target='_blank' rel='noreferrer'>
 								{post.title}
 							</a>{' '}
 						</h2>
+						{/* use mediaChooser function to select what media to display */}
 						{mediaChooser(post)}
-						<p>{post.body}</p>
+
+						{/* Display post body with link to deployed app & github repo */}
+						<p>
+							{post.body}
+							<h3>
+								<a href={post.url} target='_blank' rel='noreferrer'>
+									Link to deployed app
+								</a>
+							</h3>
+							<h3>
+								<a href={post.github} target='_blank' rel='noreferrer'>
+									Link to Github repo.
+								</a>
+							</h3>
+						</p>
 					</div>
 				);
 			})}
--- src/posts.js
+++ src/posts.js
@@ -1,15 +1,16 @@
 const posts = [
 	{
-		id: 1,
+		id: 2,
 		url: 'https://record-collection-xl.herokuapp.com/',
 		title: 'Record Collection',
 		img: null,
 		body:
 			"Record Collection is an app created to catalog and organize your music records. It's built on React, JavaScript, Django, Python and PostgreSQL.",
 		videoLink: 'https://www.youtube.com/embed/-OSjIRVcOYA',
+		github: 'https://github.com/chavierto/record-collection',
 	},
 	{
-		id: 2,
+		id: 1,
 		url: 'https://glacial-plateau-84495.herokuapp.com/',
 		title: 'Tern',
 		img:
@@ -17,6 +18,17 @@ const posts = [
 		body:
 			"Tern is a to-do app I made in collaboration with three other developers. We wanted to have a place where we could add to-do items and be able to edit them and mark them as complete. We then though about adding a number value so we could prioritize items and give them a visual reference of the priority. Finally, we added a to-do view and a completed view, in order to only see relevant to-do's. I worked primarily on the backend, where we used MongoDB, Mongoose and Express (and React on the frontend), resulting in the ability to perform full CRUD operations to the database in order to add, edit and delete to-do items.",
 		videoLink: null,
+		github: 'https://github.com/tern-app/frontend',
+	},
+	{
+		id: 0,
+		url: 'https://secure-peak-57532.herokuapp.com/',
+		title: 'Carpe Diem Dashboard',
+		img: null,
+		body:
+			"Carpe Diem is a dashboard designed to get users ready for the day. Relevant information helps users decide how to go about it by showing them the top news and an inspirational quote. Coming updates will incorporate their To-Do's a weather info panel and their schedule for a more rounded overview.",
+		videoLink: 'https://www.youtube.com/embed/CslKNkWcGa4',
+		github: 'https://github.com/chavierto/carpe-diem',
 	},
 ];
 
